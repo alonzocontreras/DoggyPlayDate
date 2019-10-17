34a3aa96012f9eeedf04cd33e8879c453ec2cfc2
Alonzo <alonzocontreras14@gmail.com>
2019-10-17 16:05:54 -0700

    starting map

--- DoggyPlay/screens/MapScreen.js
+++ DoggyPlay/screens/MapScreen.js
@@ -1,17 +1,15 @@
-import React, {Component} from 'react';
-import {
-	Stylesheet, 
-	View} from 'react-native';
+import React, { Component } from 'react';
+import { Stylesheet, View, Text, Dimensions } from 'react-native';
 import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
 
 export default class MainScreen extends Component{
 	render(){
 		return(
-			<MapView style={{flex: 1}} />
+			<MapView 
+			   style={{flex: 1}}
+		       region={this.onRegionChange}
+			/>
 		)
 	}
 }
-
-
-
-
