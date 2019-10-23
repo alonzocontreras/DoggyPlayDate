c86ddde1aa6a28c5ad219eacbf123f2a24b9110a
Hashim K Jacobs <granulargodd@sfs-wifi-dhcp-10-143-34-179.wifi.sfsu.edu>
2019-10-23 14:08:34 -0700

    Home screen

--- DoggyPlay/screens/HomeScreen.js
+++ DoggyPlay/screens/HomeScreen.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { ScrollView, StyleSheet, View, Dimensions, Text } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  View,
+  Dimensions,
+  Text,
+  Image
+} from "react-native";
 import Carousel from "react-native-snap-carousel";
 import MenuToggle from "../components/MenuToggle";
 import Header from "../components/Header";
@@ -7,10 +14,32 @@ import Header from "../components/Header";
 export default class HomeScreen extends Component {
   screenWidth = Math.round(Dimensions.get("window").width);
 
+  data = [
+    {
+      title: "Who let the dogs out?!",
+      author: 'Alonzo "The Truth" Contreras',
+      image_url: "../assets/images/dog-fight.jpeg",
+      article:
+        "For real bro, who let the damn dogs out again? Look at those things! Those are fucking wolves, dude!! Like what the hell are they even doing out here? This is crazy man, like I’ma be at home making my own street tacos and hear some motherfucking wolves fighting outside in the Sunset? Brooooo, hell to the no. You know what..."
+    },
+    {
+      title: "",
+      author: "",
+      image_url: "",
+      article: ""
+    },
+    {
+      title: "",
+      author: "",
+      image_url: "",
+      article: ""
+    }
+  ];
+
   _renderItem({ item, index }) {
     return (
       <View style={styles.slide}>
-
+        <Image source={require("../assets/images/dog-fight.jpeg")} />
       </View>
     );
   }
@@ -21,41 +50,15 @@ export default class HomeScreen extends Component {
         <MenuToggle navigation={this.props.navigation} />
         <Header title="News" />
         <ScrollView contentContainerStyle={styles.container}>
-          <Carousel
-            data={data}
-            renderItem={this._renderItem}
-            autoplay={true}
-            enableSnap={true}
-            loop={true}
-            sliderWidth={this.screenWidth}
-            itemWidth={(this.screenWidth * 9) / 10}
-          />
+          <View style={styles.carousel}>
+            <Image source={require("../assets/images/dog-fight.jpeg")} />
+            <Text>{this.data[0].article}</Text>
+          </View>
         </ScrollView>
       </View>
     );
   }
 }
-
-const data = [
-  {
-    title: "Who let the dogs out?!",
-    author: 'Alonzo "The Truth" Contreras',
-    image_url: "",
-    article: "For real bro, who let the damn dogs out again? Look at those things! Those are fucking wolves, dude!! Like what the hell are they even doing out here? This is crazy man, like I’ma be at home making my own street tacos and hear some motherfucking wolves fighting outside in the Sunset? Brooooo, hell to the no. You know what..."
-  },
-  {
-    title: "",
-    author: "",
-    image_url: "",
-    article: ""
-  },
-  {
-    title: "",
-    author: "",
-    image_url: "",
-    article: ""
-  }
-];
 
 const styles = StyleSheet.create({
   container: {
@@ -63,5 +66,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#F5FCFF",
     width: "100%"
+  },
+  carousel: {
+    flex: 1,
+    width: "80%"
   }
 });
